f44c09fbfb069058980a1d08e9e07afd0d8e3dc3
Eduardo Vicente <eduardomcv42@gmail.com>
2022-12-22 00:31:09 +0000

    Finish day01a

--- day01a/src/main.rs
+++ day01a/src/main.rs
@@ -1,3 +1,29 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("./src/input.txt").expect("Error opening file!");
+
+    let reader = BufReader::new(file);
+    let lines = reader.lines();
+
+    let mut max_found = 0;
+    let mut accumulator = 0;
+
+    for line in lines {
+        let line = line.unwrap();
+
+        if line.is_empty() {
+            if accumulator > max_found {
+                max_found = accumulator;
+            }
+            accumulator = 0;
+        } else {
+            let calories: i32 = line.parse().unwrap();
+            accumulator += calories;
+        }
+    }
+
+    println!("Maximum calories: {}", max_found);
 }
